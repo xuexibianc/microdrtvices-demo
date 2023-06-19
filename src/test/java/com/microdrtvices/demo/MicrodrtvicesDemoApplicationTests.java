1c33bb69fabbf8a121ff3f7cde8fadab32f5be32
86186 <1325963774@qq.com>
2023-06-19 19:18:39 +0800

    2023-6-19 Microdrtvices项目初始化 第一次提交

---
+++ src/test/java/com/microdrtvices/demo/MicrodrtvicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microdrtvices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrodrtvicesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
